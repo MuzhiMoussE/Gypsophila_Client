f0f7f9a16d42ab33b51ca034bb9058abe2e21380
MoussE <1258797093@qq.com>
2024-03-10 12:03:48 +0800

    【功能】读取图片序列渲染sprite

--- Assets/Scripts/Player/Sketchman.cs
+++ Assets/Scripts/Player/Sketchman.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Sketchman : MonoBehaviour
 {
+    [SerializeField] private GameObject spriteObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SketchUtility.Instance.SketchInit(spriteObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         SketchSystem.Instance.CopyActionToSkecthMan(gameObject);
+        SketchUtility.Instance.StartReleasing();
     }
 }
